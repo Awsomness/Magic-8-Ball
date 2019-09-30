ba07f79348b679507e39ea1de8897e7e8eb3ab00
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-09-30 09:35:15 -0400

    Added "1 to 100" statement to main method

---
+++ src/Magic8Ball.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[]args) {
+        Scanner keyboard;
+        int value;
+        int testing;
+        String question;
+
+
+        //initialization phase
+        keyboard = new Scanner(System.in);
+        System.out.println("Think of a question and give me a number between 1 and 100");
+
+
+
+
+
+    }
+
+}
