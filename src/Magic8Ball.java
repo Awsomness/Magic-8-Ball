04cc973e6d50e4c5ad2bf4bb5c7743eee96c6de4
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-10-03 09:38:51 -0400

    Added"positive statements" to main method

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -11,34 +11,34 @@ public class Magic8Ball {
         System.out.println("Think of a question and give me a number between 1 and 100");
         int firstNumber = keyboard.nextInt();
 
-        if (10 >= firstNumber && firstNumber >0) {
+        if (10 >= firstNumber && firstNumber >0){
             System.out.println("Never");
         }
-        else if (20 >= firstNumber && firstNumber >10) {
+        else if (20 >= firstNumber && firstNumber >10){
             System.out.println("Of course");
         }
-        else if (30 >= firstNumber && firstNumber >20) {
+        else if (30 >= firstNumber && firstNumber >20){
             System.out.println("The outlook looks good");
         }
-        else if (40 >= firstNumber && firstNumber >30) {
+        else if (40 >= firstNumber && firstNumber >30){
             System.out.println("NO");
         }
-        else if (50 >= firstNumber && firstNumber >40) {
+        else if (50 >= firstNumber && firstNumber >40){
             System.out.println("It looks promising");
         }
-        else if (60 >= firstNumber && firstNumber >50) {
+        else if (60 >= firstNumber && firstNumber >50){
             System.out.println("It does not look promising");
         }
-        else if (70 >= firstNumber && firstNumber >60) {
+        else if (70 >= firstNumber && firstNumber >60){
             System.out.println("The outlook seems pretty bad");
         }
-        else if (80 >= firstNumber && firstNumber >70) {
+        else if (80 >= firstNumber && firstNumber >70){
             System.out.println("I feel very confident in this");
         }
-        else if (90 >= firstNumber && firstNumber >80) {
+        else if (90 >= firstNumber && firstNumber >80){
             System.out.println("You don't want to know this answer");
         }
-        else if (100 >= firstNumber && firstNumber >90) {
+        else if (100 >= firstNumber && firstNumber >90){
             System.out.println("Yes");
         }
         else System.out.println("LoL that got you nowhere");
