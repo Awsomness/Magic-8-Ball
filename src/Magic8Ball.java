40d01bbc6d6574728978f00ed81c4549489fe654
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-10-03 09:06:12 -0400

    Deleted integers "value" and "testing"from main method

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -3,15 +3,45 @@ import java.util.Scanner;
 public class Magic8Ball {
 
     public static void main(String[]args) {
+        //declare
         Scanner keyboard;
-        int value;
-        int testing;
-        String question;
-
 
         //initialization phase
         keyboard = new Scanner(System.in);
         System.out.println("Think of a question and give me a number between 1 and 100");
+        int firstNumber = keyboard.nextInt();
+
+        if (10 >= firstNumber && firstNumber >0) {
+            System.out.println("Yes");
+        }
+        else if (20 >= firstNumber && firstNumber >10) {
+            System.out.println("");
+        }
+        else if (30 >= firstNumber && firstNumber >20) {
+            System.out.println("The outlook looks good");
+        }
+        else if (40 >= firstNumber && firstNumber >30) {
+            System.out.println("");
+        }
+        else if (50 >= firstNumber && firstNumber >40) {
+            System.out.println("It looks promising");
+        }
+        else if (60 >= firstNumber && firstNumber >50) {
+            System.out.println("");
+        }
+        else if (70 >= firstNumber && firstNumber >60) {
+            System.out.println("");
+        }
+        else if (80 >= firstNumber && firstNumber >70) {
+            System.out.println("I feel very confident in this");
+        }
+        else if (90 >= firstNumber && firstNumber >80) {
+            System.out.println("");
+        }
+        else if (100 >= firstNumber && firstNumber >90) {
+            System.out.println("");
+        }
+        else System.out.println("LoL that got you nowhere");
 
 
 
